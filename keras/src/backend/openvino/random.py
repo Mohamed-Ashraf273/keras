8e800f5a417cab67ea92559c4a933e38564ae1b7
Mohamed-Ashraf273 <ma2736666@gmail.com>
2025-07-01 16:04:15 +0300

    support categorical

--- keras/src/backend/openvino/random.py
+++ keras/src/backend/openvino/random.py
@@ -6,6 +6,7 @@ from keras.src.backend.config import floatx
 from keras.src.backend.openvino.core import OPENVINO_DTYPES
 from keras.src.backend.openvino.core import OpenVINOKerasTensor
 from keras.src.backend.openvino.core import convert_to_numpy
+from keras.src.backend.openvino.core import get_ov_output
 from keras.src.random.seed_generator import SeedGenerator
 from keras.src.random.seed_generator import draw_seed
 from keras.src.random.seed_generator import make_default_seed
@@ -31,7 +32,9 @@ def uniform(shape, minval=0.0, maxval=1.0, dtype=None, seed=None):
     maxval_const = ov_opset.constant(maxval, dtype=dtype)
     if isinstance(shape, tuple):
         shape = list(shape)
-    output_shape_const = ov_opset.constant(shape, dtype=Type.i32)
+    output_shape_const = ov_opset.constant(
+        shape, dtype=Type.i32
+    )
     random_uniform = ov_opset.random_uniform(
         output_shape_const, minval_const, maxval_const, ov_type, seed1, seed2
     )
@@ -39,9 +42,69 @@ def uniform(shape, minval=0.0, maxval=1.0, dtype=None, seed=None):
 
 
 def categorical(logits, num_samples, dtype="int64", seed=None):
-    raise NotImplementedError(
-        "`categorical` is not supported with openvino backend"
+    def get_shape_dims(x):
+        shape = ov_opset.shape_of(x, Type.i32)
+        rank_tensor = ov_opset.shape_of(shape, Type.i32)
+        rank_scalar = ov_opset.squeeze(
+            rank_tensor, ov_opset.constant([0], Type.i32)
+        )
+        indices = ov_opset.range(
+            ov_opset.constant(0, Type.i32),
+            rank_scalar,
+            ov_opset.constant(1, Type.i32),
+            output_type=Type.i32,
+        )
+        return ov_opset.gather(shape, indices, axis=0)
+
+    dtype = dtype or "int64"
+    ov_dtype = OPENVINO_DTYPES[dtype]
+    logits = get_ov_output(logits)
+    probs = ov_opset.softmax(logits, axis=-1)
+    cumsum_probs = ov_opset.cumsum(
+        probs, ov_opset.constant(-1, dtype="int32")
     )
+    shape = get_shape_dims(logits)
+    rank_tensor = ov_opset.shape_of(shape, Type.i32)
+    rank = ov_opset.squeeze(rank_tensor, ov_opset.constant([0], dtype=Type.i32))
+    rank_minus_1 = ov_opset.subtract(rank, ov_opset.constant(1, dtype=Type.i32))
+    indices = ov_opset.range(
+        ov_opset.constant(0, dtype=Type.i32),
+        rank_minus_1,
+        ov_opset.constant(1, dtype=Type.i32),
+        output_type=Type.i32,
+    )
+    batch_shape = ov_opset.gather(shape, indices, axis=0)
+    final_shape = ov_opset.concat(
+        [batch_shape, ov_opset.constant([num_samples], dtype=Type.i32)], axis=0
+    )
+    seed_tensor = draw_seed(seed)
+    if isinstance(seed_tensor, OpenVINOKerasTensor):
+        seed1, seed2 = convert_to_numpy(seed_tensor)
+    else:
+        seed1, seed2 = seed_tensor.data
+    rand = ov_opset.random_uniform(
+        final_shape,
+        ov_opset.constant(
+            0.0, dtype=probs.get_element_type()
+        ),
+        ov_opset.constant(
+            1.0, dtype=probs.get_element_type()
+        ),
+        probs.get_element_type(),
+        seed1,
+        seed2,
+    )
+    rand = ov_opset.unsqueeze(rand, [-1])
+    cumsum_probs = ov_opset.unsqueeze(
+        cumsum_probs, ov_opset.constant([1], dtype=Type.i32)
+    )
+    greater = ov_opset.greater(rand, cumsum_probs)
+    samples = ov_opset.reduce_sum(
+        ov_opset.convert(greater, Type.i32),
+        ov_opset.constant([-1], dtype=Type.i32),
+    )
+    samples = ov_opset.convert(samples, ov_dtype)
+    return OpenVINOKerasTensor(samples.output(0))
 
 
 def randint(shape, minval, maxval, dtype="int32", seed=None):
